4bcb361402950012134d1bd0beccd3b8f385931b
Xiao LIU <xiao.liu.contact@gmail.com>
2023-02-05 02:33:48 -0500

    add test fixtures and two test cases (for create accounts)

--- labs/03_test_fixtures/tests/test_account.py
+++ labs/03_test_fixtures/tests/test_account.py
@@ -5,6 +5,7 @@ import json
 from unittest import TestCase
 from models import db
 from models.account import Account
+import random
 
 ACCOUNT_DATA = {}
 
@@ -14,18 +15,40 @@ class TestAccountModel(TestCase):
     @classmethod
     def setUpClass(cls):
         """ Connect and load data needed by tests """
+        db.create_all()
+        global ACCOUNT_DATA
+        with open('tests/fixtures/account_data.json') as json_data:
+            ACCOUNT_DATA = json.load(json_data)
 
     @classmethod
     def tearDownClass(cls):
         """Disconnect from database"""
+        db.session.close()
 
     def setUp(self):
         """Truncate the tables"""
+        db.session.query(Account).delete()
+        db.session.commit()
 
     def tearDown(self):
         """Remove the session"""
+        db.session.remove()
 
     ######################################################################
     #  T E S T   C A S E S
     ######################################################################
 
+    def test_creat_an_account(self):
+        ''' Test create a single Account '''
+        seed=random.randint(0, len(ACCOUNT_DATA)) # get a random account in the json_data
+        data = ACCOUNT_DATA[seed]
+        account = Account(**data)
+        account.create()
+        self.assertEqual(len(Account.all()),1)
+
+    def test_creat_all_account(self):
+        ''' Test create a single Account '''
+        for data in ACCOUNT_DATA:
+            account = Account(**data)
+            account.create()
+        self.assertEqual(len(Account.all()),len(ACCOUNT_DATA))
