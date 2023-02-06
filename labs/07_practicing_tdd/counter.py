6c0d1fe2c8517ea003f71963a5508d7857d70423
Xiao LIU <xiao.liu.contact@gmail.com>
2023-02-06 06:49:13 -0500

    Create counter.py using flask

--- labs/07_practicing_tdd/counter.py
+++ labs/07_practicing_tdd/counter.py
@@ -16,3 +16,41 @@ def create_counter(name):
 
     COUNTERS[name] = 0
     return { name: COUNTERS[name] }, status.HTTP_201_CREATED
+
+@app.route("/counters/<name>", methods=["PUT"])
+def update_counter(name):
+    """Update a counter"""
+    app.logger.info(f"Request to Update counter: {name}")
+    global COUNTERS
+
+    if name not in COUNTERS:
+        return {"message":f"Counter {name} dosen't exist"}, status.HTTP_404_NOT_FOUND
+
+    COUNTERS[name] += 1
+    return { name: COUNTERS[name] }, status.HTTP_200_OK
+
+@app.route("/counters/<name>", methods=["GET"])
+def read_counter(name):
+    """Read a counter"""
+    app.logger.info(f"Request to Read counter: {name}")
+    global COUNTERS
+
+    if name not in COUNTERS:
+        return {"message":f"Counter {name} dosen't exist"}, status.HTTP_404_NOT_FOUND
+
+    return { name: COUNTERS[name] }, status.HTTP_200_OK
+
+@app.route("/counters/<name>", methods=["DELETE"])
+def delete_counter(name):
+    """Delete a counter"""
+    app.logger.info(f"Request to Delete counter: {name}")
+    app.logger.info(f"Counter: {name} has been deleted")
+    global COUNTERS
+    
+    if name not in COUNTERS:
+        return {"message":f"Counter {name} dosen't exist"}, status.HTTP_404_NOT_FOUND
+
+    # delete the count
+    del(COUNTERS[name])
+
+    return '', status.HTTP_204_NO_CONTENT
