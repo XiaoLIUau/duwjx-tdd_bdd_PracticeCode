eec6dbfd08a40902986d0303e45320826c15e9a0
Xiao LIU <xiao.liu.contact@gmail.com>
2023-02-06 06:49:39 -0500

    Create test cases for counter.py file

--- labs/07_practicing_tdd/test_counter.py
+++ labs/07_practicing_tdd/test_counter.py
@@ -25,3 +25,48 @@ class CounterTest(TestCase):
         self.assertEqual(result.status_code, status.HTTP_201_CREATED)
         result = self.client.post("/counters/bar")
         self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_read_counter(self):
+        """It should read the counter"""
+        result = self.client.post("/counters/aa")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        # read the counter
+        result = self.client.get("/counters/aa")
+        read = result.get_json()
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertEqual(read['aa'],0)
+
+    def test_read_counter_cannot_find(self):
+        """Read a counter that cannot find - return 404"""
+        result = self.client.get("/counters/bb")
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_counter(self):
+        """Detele a counter """
+        result = self.client.post("/counters/bb")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        # delete the counter
+        result = self.client.delete("/counters/bb")
+        self.assertEqual(result.status_code, status.HTTP_204_NO_CONTENT)
+        
+    def test_delete_counter_cannot_find(self):
+        """Detele a counter that cannot find - return 404"""
+        result = self.client.delete("/counters/bb")
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_update_counter(self):
+        """It should update the counter"""
+        self.client.delete("/counters/aa")
+        result = self.client.post("/counters/aa")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        data = result.get_json()
+        baseline = data['aa']
+        result = self.client.put("/counters/aa")
+        update = result.get_json()
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertEqual(baseline+1,update['aa'])
+
+    def test_update_counter_cannot_find(self):
+        """Update a counter that cannot find - return 404"""
+        result = self.client.put("/counters/bb")
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
